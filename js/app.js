6d3e8f3027727ed480b68cf244f217a52e322263
Christopher Clark <cclark@sfsu.edu>
2014-03-08 19:01:05 -0800

    AngularJS, Part 2
    Flesh out partials, add AngularJS animate

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('breakroomApp', ['ngRoute'])
+angular.module('breakroomApp', ['ngRoute','ngAnimate'])
     .config(breakroomRouter);
 
 function breakroomRouter ($routeProvider)
@@ -29,4 +29,19 @@ function navController($scope, $location)
     $scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
+}
+
+// request JSON feed from delicious account
+function deliciousCtrl ($scope, $http) {
+    $http.jsonp('http://feeds.delicious.com/v2/json/modified65?count=5&callback=JSON_CALLBACK').then(function (response) {
+          $scope.entries = response.data;
+    });
+}
+
+
+// request JSON feed
+function wordpressCtrl ($scope, $http) {
+    $http.jsonp('http://localhost:8888/breakroom-wp/?json=1').then(function (response) {
+          $scope.posts = response.data;
+    });
 }
